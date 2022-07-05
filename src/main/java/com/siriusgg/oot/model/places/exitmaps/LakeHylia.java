8be8f008e2dbd056b65427672158c4e80f526f05
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2022-07-05 21:15:35 +0200

    FEAT: Added Exit objects for Lake Hylia

--- src/main/java/com/siriusgg/oot/model/places/exitmaps/LakeHylia.java
+++ src/main/java/com/siriusgg/oot/model/places/exitmaps/LakeHylia.java
@@ -14,5 +14,65 @@ public class LakeHylia extends ExitMap {
         setOverworldTransitionsAmount(1);
         setHasOwlStart(true);
         initExits();
+        setExit(new LakeHyliaToFishingHole(), 0);
+        setExit(new LakeHyliaToLab(), 1);
+        setExit(new LakeHyliaToWaterTemple(), 2);
+        setExit(new LakeHyliaToGrotto(), 3);
+        setExit(new LakeHyliaToHyruleField(), 4);
+        setExit(new LakeHyliaOwlStart(), 5);
+    }
+
+    private static class LakeHyliaToFishingHole extends Exit {
+        public LakeHyliaToFishingHole() {
+            super(ExitType.DOOR_ENTRANCE);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setDoorEntrance(DoorEntrance.LH_FISHING_HOLE);
+        }
+    }
+
+    private static class LakeHyliaToLab extends Exit {
+        public LakeHyliaToLab() {
+            super(ExitType.DOOR_ENTRANCE);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setDoorEntrance(DoorEntrance.LH_LAB);
+        }
+    }
+
+    private static class LakeHyliaToWaterTemple extends Exit {
+        public LakeHyliaToWaterTemple() {
+            super(ExitType.DUNGEON_ENTRANCE);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setDungeonEntrance(DungeonEntrance.WATER_TEMPLE);
+        }
+    }
+
+    private static class LakeHyliaToGrotto extends Exit {
+        public LakeHyliaToGrotto() {
+            super(ExitType.GROTTO_ENTRANCE);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setGrottoEntrance(GrottoEntrance.LH_GROTTO);
+        }
+    }
+
+    private static class LakeHyliaToHyruleField extends Exit {
+        public LakeHyliaToHyruleField() {
+            super(ExitType.OVERWORLD);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setOverworld(Overworld.HYRULE_FIELD);
+        }
+    }
+
+    private static class LakeHyliaOwlStart extends Exit {
+        public LakeHyliaOwlStart() {
+            super(ExitType.OWL_START);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setOwlStart(OwlStart.LAKE_HYLIA);
+        }
     }
 }
