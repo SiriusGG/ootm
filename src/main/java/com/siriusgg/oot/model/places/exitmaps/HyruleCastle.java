a67142a64e5f70ad729ea1679c8918e32ec270b1
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2022-07-05 19:37:11 +0200

    FEAT: Added Exit objects for Hyrule Castle and Outside Ganon's Castle (shared map)

--- src/main/java/com/siriusgg/oot/model/places/exitmaps/HyruleCastle.java
+++ src/main/java/com/siriusgg/oot/model/places/exitmaps/HyruleCastle.java
@@ -12,6 +12,67 @@ public class HyruleCastle extends ExitMap {
         setDungeonEntrancesAmount(1);
         setGrottoEntrancesAmount(1);
         setOverworldTransitionsAmount(1);
+        setUnchangingTransitionsAmount(1);
         initExits();
+        setExit(new HCToHCGreatFairy(), 0);
+        setExit(new OGCToOGCGreatFairy(), 1);
+        setExit(new OGCToInsideGanonsCastle(), 2);
+        setExit(new HCToStormsGrotto(), 3);
+        setExit(new HCToMarket(), 4);
+        setExit(new HCToCastleGarden(), 5);
+    }
+
+    private static class HCToHCGreatFairy extends Exit {
+        public HCToHCGreatFairy() {
+            super(ExitType.DOOR_ENTRANCE);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(false);
+            setDoorEntrance(DoorEntrance.HC_GREAT_FAIRY_FOUNTAIN);
+        }
+    }
+
+    private static class OGCToOGCGreatFairy extends Exit {
+        public OGCToOGCGreatFairy() {
+            super(ExitType.DOOR_ENTRANCE);
+            intendedAccessibleAsChild(false);
+            intendedAccessibleAsAdult(true);
+            setDoorEntrance(DoorEntrance.OGC_GREAT_FAIRY_FOUNTAIN);
+        }
+    }
+
+    private static class OGCToInsideGanonsCastle extends Exit {
+        public OGCToInsideGanonsCastle() {
+            super(ExitType.DUNGEON_ENTRANCE);
+            intendedAccessibleAsChild(false);
+            intendedAccessibleAsAdult(true);
+            setDungeonEntrance(DungeonEntrance.INSIDE_GANONS_CASTLE);
+        }
+    }
+
+    private static class HCToStormsGrotto extends Exit {
+        public HCToStormsGrotto() {
+            super(ExitType.GROTTO_ENTRANCE);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(false);
+            setGrottoEntrance(GrottoEntrance.HC_STORMS_GROTTO);
+        }
+    }
+
+    private static class HCToMarket extends Exit {
+        public HCToMarket() {
+            super(ExitType.OVERWORLD);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setOverworld(Overworld.MARKET);
+        }
+    }
+
+    private static class HCToCastleGarden extends Exit {
+        public HCToCastleGarden() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(false);
+            setUnchangingTransition(UnchangingTransition.HYRULE_CASTLE_TO_CASTLE_GARDEN);
+        }
     }
 }
