5c479168d45e96ef39a0ef6b490c1f79a6beaf74
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2022-07-05 19:08:49 +0200

    FEAT: Added Exit objects for Graveyard

--- src/main/java/com/siriusgg/oot/model/places/exitmaps/Graveyard.java
+++ src/main/java/com/siriusgg/oot/model/places/exitmaps/Graveyard.java
@@ -13,6 +13,13 @@ public class Graveyard extends ExitMap {
         setGrottoEntrancesAmount(4);
         setOverworldTransitionsAmount(1);
         initExits();
+        setExit(new GraveyardToDampesHouse(), 0);
+        setExit(new GraveyardToShadowTemple(), 1);
+        setExit(new GraveyardToShieldGrave(), 2);
+        setExit(new GraveyardToHeartPieceGrave(), 3);
+        setExit(new GraveyardToRoyalFamilyTomb(), 4);
+        setExit(new GraveyardToDampesGrave(), 5);
+        setExit(new GraveyardToKakarikoVillage(), 6);
     }
 
     private static class GraveyardToDampesHouse extends Exit {
@@ -30,6 +37,51 @@ public class Graveyard extends ExitMap {
             intendedAccessibleAsChild(true);
             intendedAccessibleAsAdult(true);
             setDungeonEntrance(DungeonEntrance.SHADOW_TEMPLE);
+        }
+    }
+
+    private static class GraveyardToShieldGrave extends Exit {
+        public GraveyardToShieldGrave() {
+            super(ExitType.GROTTO_ENTRANCE);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setGrottoEntrance(GrottoEntrance.GRAVEYARD_SHIELD_GRAVE);
+        }
+    }
+
+    private static class GraveyardToHeartPieceGrave extends Exit {
+        public GraveyardToHeartPieceGrave() {
+            super(ExitType.GROTTO_ENTRANCE);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setGrottoEntrance(GrottoEntrance.GRAVEYARD_HEART_PIECE_GRAVE);
+        }
+    }
+
+    private static class GraveyardToRoyalFamilyTomb extends Exit {
+        public GraveyardToRoyalFamilyTomb() {
+            super(ExitType.GROTTO_ENTRANCE);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setGrottoEntrance(GrottoEntrance.GRAVEYARD_ROYAL_FAMILYS_TOMB);
+        }
+    }
+
+    private static class GraveyardToDampesGrave extends Exit {
+        public GraveyardToDampesGrave() {
+            super(ExitType.GROTTO_ENTRANCE);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setGrottoEntrance(GrottoEntrance.DAMPES_GRAVE);
+        }
+    }
+
+    private static class GraveyardToKakarikoVillage extends Exit {
+        public GraveyardToKakarikoVillage() {
+            super(ExitType.OVERWORLD);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setOverworld(Overworld.KAKARIKO_VILLAGE);
         }
     }
 }
