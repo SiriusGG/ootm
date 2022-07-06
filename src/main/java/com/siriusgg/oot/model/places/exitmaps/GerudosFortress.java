d96b5f9b13c23ae3dd3ee63a82e8d30a8bf4f806
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2022-07-06 20:24:01 +0200

    FEAT: Added Exit objects for Thieves Hideout

--- src/main/java/com/siriusgg/oot/model/places/exitmaps/GerudosFortress.java
+++ src/main/java/com/siriusgg/oot/model/places/exitmaps/GerudosFortress.java
@@ -11,12 +11,24 @@ public class GerudosFortress extends ExitMap {
         setDungeonEntrancesAmount(1);
         setGrottoEntrancesAmount(1);
         setOverworldTransitionsAmount(2);
-        // ToDo: Add Thief's Hideout entrances as unchanging transitions
+        setUnchangingTransitionsAmount(13);
         setAccessibility(false, true);
         initExits();
         setExit(new GerudosFortressToGerudoTrainingGround(), 0);
         setExit(new GerudosFortressToStormsGrotto(), 1);
-        // ToDo: Add Thief's Hideout entrances
+        setExit(new GerudosFortressToThievesHideoutB(), 2);
+        setExit(new GerudosFortressToThievesHideoutC(), 3);
+        setExit(new GerudosFortressToThievesHideoutD(), 4);
+        setExit(new GerudosFortressToThievesHideoutE(), 5);
+        setExit(new GerudosFortressToThievesHideoutF(), 6);
+        setExit(new GerudosFortressToThievesHideoutG(), 7);
+        setExit(new GerudosFortressToThievesHideoutH(), 8);
+        setExit(new GerudosFortressToThievesHideoutI(), 9);
+        setExit(new GerudosFortressToThievesHideoutJ(), 10);
+        setExit(new GerudosFortressToThievesHideoutK(), 11);
+        setExit(new GerudosFortressToThievesHideoutL(), 12);
+        setExit(new GerudosFortressToThievesHideoutM(), 13);
+        setExit(new GerudosFortressToThievesHideoutN(), 14);
     }
 
     private static class GerudosFortressToGerudoTrainingGround extends Exit {
@@ -37,5 +49,120 @@ public class GerudosFortress extends ExitMap {
         }
     }
 
-    // ToDo: Add Thief's Hideout entrances as unchanging transitions
+    private static class GerudosFortressToThievesHideoutB extends Exit {
+        public GerudosFortressToThievesHideoutB() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_B_IN);
+        }
+    }
+
+    private static class GerudosFortressToThievesHideoutC extends Exit {
+        public GerudosFortressToThievesHideoutC() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_C_IN);
+        }
+    }
+
+    private static class GerudosFortressToThievesHideoutD extends Exit {
+        public GerudosFortressToThievesHideoutD() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_D_IN);
+        }
+    }
+
+    private static class GerudosFortressToThievesHideoutE extends Exit {
+        public GerudosFortressToThievesHideoutE() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_E_IN);
+        }
+    }
+
+    private static class GerudosFortressToThievesHideoutF extends Exit {
+        public GerudosFortressToThievesHideoutF() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_F_IN);
+        }
+    }
+
+    private static class GerudosFortressToThievesHideoutG extends Exit {
+        public GerudosFortressToThievesHideoutG() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_G_IN);
+        }
+    }
+
+    private static class GerudosFortressToThievesHideoutH extends Exit {
+        public GerudosFortressToThievesHideoutH() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_H_IN);
+        }
+    }
+
+    private static class GerudosFortressToThievesHideoutI extends Exit {
+        public GerudosFortressToThievesHideoutI() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_I_IN);
+        }
+    }
+
+    private static class GerudosFortressToThievesHideoutJ extends Exit {
+        public GerudosFortressToThievesHideoutJ() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_J_IN);
+        }
+    }
+
+    private static class GerudosFortressToThievesHideoutK extends Exit {
+        public GerudosFortressToThievesHideoutK() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_K_IN);
+        }
+    }
+
+    private static class GerudosFortressToThievesHideoutL extends Exit {
+        public GerudosFortressToThievesHideoutL() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_L_IN);
+        }
+    }
+
+    private static class GerudosFortressToThievesHideoutM extends Exit {
+        public GerudosFortressToThievesHideoutM() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_M_IN);
+        }
+    }
+
+    private static class GerudosFortressToThievesHideoutN extends Exit {
+        public GerudosFortressToThievesHideoutN() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_N_IN);
+        }
+    }
 }
--- src/main/java/com/siriusgg/oot/model/places/exitmaps/ThievesHideout.java
+++ src/main/java/com/siriusgg/oot/model/places/exitmaps/ThievesHideout.java
@@ -8,7 +8,137 @@ public class ThievesHideout extends ExitMap {
         super(settings);
         setName("Thieves' Hideout");
         initMap();
-        setDoorExitsAmount(50); // ToDo: Count
+        setDoorExitsAmount(13);
         initExits();
+        setExit(new ThievesHideoutBToGerudosFortress(), 0);
+        setExit(new ThievesHideoutCToGerudosFortress(), 1);
+        setExit(new ThievesHideoutDToGerudosFortress(), 2);
+        setExit(new ThievesHideoutEToGerudosFortress(), 3);
+        setExit(new ThievesHideoutFToGerudosFortress(), 4);
+        setExit(new ThievesHideoutGToGerudosFortress(), 5);
+        setExit(new ThievesHideoutHToGerudosFortress(), 6);
+        setExit(new ThievesHideoutIToGerudosFortress(), 7);
+        setExit(new ThievesHideoutJToGerudosFortress(), 8);
+        setExit(new ThievesHideoutKToGerudosFortress(), 9);
+        setExit(new ThievesHideoutLToGerudosFortress(), 10);
+        setExit(new ThievesHideoutMToGerudosFortress(), 11);
+        setExit(new ThievesHideoutNToGerudosFortress(), 12);
+    }
+
+    private static class ThievesHideoutBToGerudosFortress extends Exit {
+        public ThievesHideoutBToGerudosFortress() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_B_OUT);
+        }
+    }
+
+    private static class ThievesHideoutCToGerudosFortress extends Exit {
+        public ThievesHideoutCToGerudosFortress() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_C_OUT);
+        }
+    }
+
+    private static class ThievesHideoutDToGerudosFortress extends Exit {
+        public ThievesHideoutDToGerudosFortress() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_D_OUT);
+        }
+    }
+
+    private static class ThievesHideoutEToGerudosFortress extends Exit {
+        public ThievesHideoutEToGerudosFortress() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_E_OUT);
+        }
+    }
+
+    private static class ThievesHideoutFToGerudosFortress extends Exit {
+        public ThievesHideoutFToGerudosFortress() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_F_OUT);
+        }
+    }
+
+    private static class ThievesHideoutGToGerudosFortress extends Exit {
+        public ThievesHideoutGToGerudosFortress() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_G_OUT);
+        }
+    }
+
+    private static class ThievesHideoutHToGerudosFortress extends Exit {
+        public ThievesHideoutHToGerudosFortress() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_H_OUT);
+        }
+    }
+
+    private static class ThievesHideoutIToGerudosFortress extends Exit {
+        public ThievesHideoutIToGerudosFortress() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_I_OUT);
+        }
+    }
+
+    private static class ThievesHideoutJToGerudosFortress extends Exit {
+        public ThievesHideoutJToGerudosFortress() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_J_OUT);
+        }
+    }
+
+    private static class ThievesHideoutKToGerudosFortress extends Exit {
+        public ThievesHideoutKToGerudosFortress() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_K_OUT);
+        }
+    }
+
+    private static class ThievesHideoutLToGerudosFortress extends Exit {
+        public ThievesHideoutLToGerudosFortress() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_L_OUT);
+        }
+    }
+
+    private static class ThievesHideoutMToGerudosFortress extends Exit {
+        public ThievesHideoutMToGerudosFortress() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_M_OUT);
+        }
+    }
+
+    private static class ThievesHideoutNToGerudosFortress extends Exit {
+        public ThievesHideoutNToGerudosFortress() {
+            super(ExitType.UNCHANGING);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setUnchangingTransition(UnchangingTransition.THIEVES_HIDEOUT_N_OUT);
+        }
     }
 }
