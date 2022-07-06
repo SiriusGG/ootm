c06c9ab5e4a8d1cc9a55d33bfe59c56d0d543c79
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2022-07-06 19:35:42 +0200

    FEAT: Added Exit objects for Zora's River

--- src/main/java/com/siriusgg/oot/model/places/exitmaps/ZorasRiver.java
+++ src/main/java/com/siriusgg/oot/model/places/exitmaps/ZorasRiver.java
@@ -11,5 +11,65 @@ public class ZorasRiver extends ExitMap {
         setGrottoEntrancesAmount(3);
         setOverworldTransitionsAmount(3);
         initExits();
+        setExit(new ZorasRiverToOpenGrotto(), 0);
+        setExit(new ZorasRiverToFairyGrotto(), 1);
+        setExit(new ZorasRiverToStormsGrotto(), 2);
+        setExit(new ZorasRiverToHyruleField(), 3);
+        setExit(new ZorasRiverToZorasDomain(), 4);
+        setExit(new ZorasRiverToLostWoods(), 5);
+    }
+
+    private static class ZorasRiverToOpenGrotto extends Exit {
+        public ZorasRiverToOpenGrotto() {
+            super(ExitType.GROTTO_ENTRANCE);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setGrottoEntrance(GrottoEntrance.ZR_OPEN_GROTTO);
+        }
+    }
+
+    private static class ZorasRiverToFairyGrotto extends Exit {
+        public ZorasRiverToFairyGrotto() {
+            super(ExitType.GROTTO_ENTRANCE);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setGrottoEntrance(GrottoEntrance.ZR_FAIRY_GROTTO);
+        }
+    }
+
+    private static class ZorasRiverToStormsGrotto extends Exit {
+        public ZorasRiverToStormsGrotto() {
+            super(ExitType.GROTTO_ENTRANCE);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setGrottoEntrance(GrottoEntrance.ZR_STORMS_GROTTO);
+        }
+    }
+
+    private static class ZorasRiverToHyruleField extends Exit {
+        public ZorasRiverToHyruleField() {
+            super(ExitType.OVERWORLD);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setOverworld(Overworld.HYRULE_FIELD);
+        }
+    }
+
+    private static class ZorasRiverToZorasDomain extends Exit {
+        public ZorasRiverToZorasDomain() {
+            super(ExitType.OVERWORLD);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setOverworld(Overworld.ZORAS_DOMAIN);
+        }
+    }
+
+    private static class ZorasRiverToLostWoods extends Exit {
+        public ZorasRiverToLostWoods() {
+            super(ExitType.OVERWORLD);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setOverworld(Overworld.LOST_WOODS);
+        }
     }
 }
