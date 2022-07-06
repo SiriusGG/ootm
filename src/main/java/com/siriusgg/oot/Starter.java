f5a9f19d91240700e5520b1970993752f424d837
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2022-07-06 21:05:58 +0200

    FEAT: 'Permanently Loaded Information' (a singleton which holds constant values which are read a lot) now loads upon starting the software, rather than on first lookup. Also made preparations for an eventual loading screen banner.

--- src/main/java/com/siriusgg/oot/Starter.java
+++ src/main/java/com/siriusgg/oot/Starter.java
@@ -1,10 +1,20 @@
 package com.siriusgg.oot;
 
 import com.siriusgg.oot.controller.MainMenuController;
+import com.siriusgg.oot.model.places.PermanentlyLoadedInformation;
 
 public class Starter {
     public static void main(String[] args) {
+        showSplashscreen();
+        PermanentlyLoadedInformation.getInstance();
         MainMenuController mmc = new MainMenuController();
+        hideSplashscreen();
         mmc.init();
+    }
+
+    private static void showSplashscreen() {
+    }
+
+    private static void hideSplashscreen() {
     }
 }
