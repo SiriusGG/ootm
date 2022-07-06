7746e5192249776fba97dc192c7f67da210b8132
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2022-07-06 16:39:06 +0200

    FEAT: Added Exit objects for Lon Lon Ranch

--- src/main/java/com/siriusgg/oot/model/places/exitmaps/LonLonRanch.java
+++ src/main/java/com/siriusgg/oot/model/places/exitmaps/LonLonRanch.java
@@ -12,5 +12,55 @@ public class LonLonRanch extends ExitMap {
         setGrottoEntrancesAmount(1);
         setOverworldTransitionsAmount(1);
         initExits();
+        setExit(new LonLonRanchToTalonsHouse(), 0);
+        setExit(new LonLonRanchToStable(), 1);
+        setExit(new LonLonRanchToTower(), 2);
+        setExit(new LonLonRanchToGrotto(), 3);
+        setExit(new LonLonRanchToHyruleField(), 4);
+    }
+
+    private static class LonLonRanchToTalonsHouse extends Exit {
+        public LonLonRanchToTalonsHouse() {
+            super(ExitType.DOOR_ENTRANCE);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setDoorEntrance(DoorEntrance.LLR_TALONS_HOUSE);
+        }
+    }
+
+    private static class LonLonRanchToStable extends Exit {
+        public LonLonRanchToStable() {
+            super(ExitType.DOOR_ENTRANCE);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setDoorEntrance(DoorEntrance.LLR_STABLES);
+        }
+    }
+
+    private static class LonLonRanchToTower extends Exit {
+        public LonLonRanchToTower() {
+            super(ExitType.DOOR_ENTRANCE);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setDoorEntrance(DoorEntrance.LLR_TOWER);
+        }
+    }
+
+    private static class LonLonRanchToGrotto extends Exit {
+        public LonLonRanchToGrotto() {
+            super(ExitType.GROTTO_ENTRANCE);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(false);
+            setGrottoEntrance(GrottoEntrance.LLR_GROTTO);
+        }
+    }
+
+    private static class LonLonRanchToHyruleField extends Exit {
+        public LonLonRanchToHyruleField() {
+            super(ExitType.OVERWORLD);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setOverworld(Overworld.HYRULE_FIELD);
+        }
     }
 }
