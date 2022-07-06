0bd5d21086ab0cb345714375689635b1cf01b5ae
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2022-07-06 19:12:19 +0200

    FEAT: Added Exit objects for Zora's Fountain

--- src/main/java/com/siriusgg/oot/model/places/exitmaps/ZorasFountain.java
+++ src/main/java/com/siriusgg/oot/model/places/exitmaps/ZorasFountain.java
@@ -12,5 +12,45 @@ public class ZorasFountain extends ExitMap {
         setDungeonEntrancesAmount(2);
         setOverworldTransitionsAmount(1);
         initExits();
+        setExit(new ZorasFountainToGreatFairy(), 0);
+        setExit(new ZorasFountainToInsideJabuJabusBelly(), 1);
+        setExit(new ZorasFountainToIceCavern(), 2);
+        setExit(new ZorasFountainToZorasDomain(), 3);
+    }
+
+    private static class ZorasFountainToGreatFairy extends Exit {
+        public ZorasFountainToGreatFairy() {
+            super(ExitType.DOOR_ENTRANCE);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setDoorEntrance(DoorEntrance.ZF_GREAT_FAIRY_FOUNTAIN);
+        }
+    }
+
+    private static class ZorasFountainToInsideJabuJabusBelly extends Exit {
+        public ZorasFountainToInsideJabuJabusBelly() {
+            super(ExitType.DUNGEON_ENTRANCE);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(false);
+            setDungeonEntrance(DungeonEntrance.INSIDE_JABU_JABUS_BELLY);
+        }
+    }
+
+    private static class ZorasFountainToIceCavern extends Exit {
+        public ZorasFountainToIceCavern() {
+            super(ExitType.DUNGEON_ENTRANCE);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setDungeonEntrance(DungeonEntrance.ICE_CAVERN);
+        }
+    }
+
+    private static class ZorasFountainToZorasDomain extends Exit {
+        public ZorasFountainToZorasDomain() {
+            super(ExitType.OVERWORLD);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setOverworld(Overworld.ZORAS_DOMAIN);
+        }
     }
 }
