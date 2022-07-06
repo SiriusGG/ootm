1bfb5524dae9594a9b5f62c6412c60fba52e20b7
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2022-07-06 18:36:46 +0200

    FEAT: Added Exit objects for Temple of Time Entrance (the small path between Market and Temple of Time)

--- src/main/java/com/siriusgg/oot/model/places/exitmaps/ToTEntrance.java
+++ src/main/java/com/siriusgg/oot/model/places/exitmaps/ToTEntrance.java
@@ -8,8 +8,28 @@ public class ToTEntrance extends ExitMap {
         super(settings);
         setName("Temple of Time Entrance");
         initMap();
-        setOverworldTransitionsAmount(1);
         setDoorEntrancesAmount(1);
+        setOverworldTransitionsAmount(1);
         initExits();
+        setExit(new ToTEntranceToTempleOfTime(), 0);
+        setExit(new ToTEntranceToMarket(), 1);
+    }
+
+    private static class ToTEntranceToTempleOfTime extends Exit {
+        public ToTEntranceToTempleOfTime() {
+            super(ExitType.DOOR_ENTRANCE);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setDoorEntrance(DoorEntrance.TEMPLE_OF_TIME);
+        }
+    }
+
+    private static class ToTEntranceToMarket extends Exit {
+        public ToTEntranceToMarket() {
+            super(ExitType.OVERWORLD);
+            intendedAccessibleAsChild(true);
+            intendedAccessibleAsAdult(true);
+            setOverworld(Overworld.MARKET);
+        }
     }
 }
