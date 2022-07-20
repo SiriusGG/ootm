135e67d300d5119f9ad8b95bf81eaa193ef7a6ed
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2022-07-20 20:54:57 +0200

    FEAT: Added positions for transitions from Desert Colossus

--- src/main/java/com/siriusgg/oot/model/places/exitmaps/DesertColossus.java
+++ src/main/java/com/siriusgg/oot/model/places/exitmaps/DesertColossus.java
@@ -1,6 +1,7 @@
 package com.siriusgg.oot.model.places.exitmaps;
 
-import com.siriusgg.oot.model.PermanentlyLoadedInformation;
+import com.siriusgg.oot.exception.UnknownPerspectiveException;
+import com.siriusgg.oot.model.*;
 import com.siriusgg.oot.model.places.*;
 
 public class DesertColossus extends ExitMap {
@@ -28,6 +29,8 @@ public class DesertColossus extends ExitMap {
             intendedAccessibleAsChild(true);
             intendedAccessibleAsAdult(true);
             setDoorEntrance(DoorEntrance.COLOSSUS_GREAT_FAIRY_FOUNTAIN);
+            setBothSidePositions(new Position(67.5, 58.0));
+            setBothTopPositions(new Position(63.0, 24.0));
         }
     }
 
@@ -37,6 +40,8 @@ public class DesertColossus extends ExitMap {
             intendedAccessibleAsChild(true);
             intendedAccessibleAsAdult(true);
             setDungeonEntrance(DungeonEntrance.SPIRIT_TEMPLE);
+            setBothSidePositions(new Position(24.0, 49.8));
+            setBothTopPositions(new Position(20.0, 46.0));
         }
     }
 
@@ -46,6 +51,8 @@ public class DesertColossus extends ExitMap {
             intendedAccessibleAsChild(false);
             intendedAccessibleAsAdult(true);
             setGrottoEntrance(GrottoEntrance.COLOSSUS_GROTTO);
+            setBothSidePositions(new Position(47.3, 51.0));
+            setBothTopPositions(new Position(36.0, 27.0));
         }
     }
 
@@ -55,6 +62,8 @@ public class DesertColossus extends ExitMap {
             intendedAccessibleAsChild(true);
             intendedAccessibleAsAdult(true);
             setOverworld(Overworld.HAUNTED_WASTELAND);
+            setBothSidePositions(new Position(79.5, 83.5));
+            setBothTopPositions(new Position(89.0, 43.5));
         }
     }
 
@@ -64,6 +73,8 @@ public class DesertColossus extends ExitMap {
             intendedAccessibleAsChild(true);
             intendedAccessibleAsAdult(true);
             setUnchangingTransition(UnchangingTransition.DESERT_COLOSSUS_TO_SPIRIT_TEMPLE_LEFT_HAND);
+            setBothSidePositions(new Position(26.0, 30.0));
+            setBothTopPositions(new Position(16.0, 40.0));
         }
     }
 
@@ -73,6 +84,30 @@ public class DesertColossus extends ExitMap {
             intendedAccessibleAsChild(true);
             intendedAccessibleAsAdult(true);
             setUnchangingTransition(UnchangingTransition.DESERT_COLOSSUS_TO_SPIRIT_TEMPLE_RIGHT_HAND);
+            setBothSidePositions(new Position(18.0, 35.0));
+            setBothTopPositions(new Position(16.0, 52.0));
+        }
+    }
+
+    @Override
+    public int getPreferredButtonWidth() throws UnknownPerspectiveException {
+        if (Settings.getInstance().getPerspective() == Perspective.SIDE) {
+            return 50;
+        } else if (Settings.getInstance().getPerspective() == Perspective.TOP) {
+            return 40;
+        } else {
+            throw new UnknownPerspectiveException(Settings.getInstance().getPerspective());
+        }
+    }
+
+    @Override
+    public int getPreferredButtonHeight() throws UnknownPerspectiveException {
+        if (Settings.getInstance().getPerspective() == Perspective.SIDE) {
+            return 50;
+        } else if (Settings.getInstance().getPerspective() == Perspective.TOP) {
+            return 40;
+        } else {
+            throw new UnknownPerspectiveException(Settings.getInstance().getPerspective());
         }
     }
 }
