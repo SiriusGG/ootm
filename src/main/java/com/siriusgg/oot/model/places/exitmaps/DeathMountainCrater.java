0783a3e7d88b7e6d554b277103fea869dbcb200d
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2022-07-20 16:44:40 +0200

    FEAT: Added positions for transitions from Death Mountain Crater

--- src/main/java/com/siriusgg/oot/model/places/exitmaps/DeathMountainCrater.java
+++ src/main/java/com/siriusgg/oot/model/places/exitmaps/DeathMountainCrater.java
@@ -1,6 +1,7 @@
 package com.siriusgg.oot.model.places.exitmaps;
 
-import com.siriusgg.oot.model.PermanentlyLoadedInformation;
+import com.siriusgg.oot.exception.UnknownPerspectiveException;
+import com.siriusgg.oot.model.*;
 import com.siriusgg.oot.model.places.*;
 
 public class DeathMountainCrater extends ExitMap {
@@ -27,6 +28,8 @@ public class DeathMountainCrater extends ExitMap {
             intendedAccessibleAsChild(true);
             intendedAccessibleAsAdult(true);
             setDoorEntrance(DoorEntrance.DMC_GREAT_FAIRY_FOUNTAIN);
+            setBothSidePositions(new Position(50.5, 50.0));
+            setBothTopPositions(new Position(16.0, 60.0));
         }
     }
 
@@ -36,6 +39,8 @@ public class DeathMountainCrater extends ExitMap {
             intendedAccessibleAsChild(true);
             intendedAccessibleAsAdult(true);
             setDungeonEntrance(DungeonEntrance.FIRE_TEMPLE);
+            setBothSidePositions(new Position(79.5, 83.5));
+            setBothTopPositions(new Position(44.0, 3.0));
         }
     }
 
@@ -45,15 +50,19 @@ public class DeathMountainCrater extends ExitMap {
             intendedAccessibleAsChild(true);
             intendedAccessibleAsAdult(true);
             setGrottoEntrance(GrottoEntrance.DMC_UPPER_GROTTO);
+            setBothSidePositions(new Position(23.5, 43.0));
+            setBothTopPositions(new Position(44.8, 75.0));
         }
     }
 
     private static class DMCToDMCHammerGrotto extends Exit {
         public DMCToDMCHammerGrotto() {
             super(ExitType.GROTTO_ENTRANCE);
-            intendedAccessibleAsChild(true);
+            intendedAccessibleAsChild(false);
             intendedAccessibleAsAdult(true);
             setGrottoEntrance(GrottoEntrance.DMC_HAMMER_GROTTO);
+            setBothSidePositions(new Position(73.0, 60.5));
+            setBothTopPositions(new Position(12.5, 30.0));
         }
     }
 
@@ -63,6 +72,8 @@ public class DeathMountainCrater extends ExitMap {
             intendedAccessibleAsChild(true);
             intendedAccessibleAsAdult(true);
             setOverworld(Overworld.DEATH_MOUNTAIN_TRAIL);
+            setBothSidePositions(new Position(32.0, 32.0));
+            setBothTopPositions(new Position(3.5, 93.7));
         }
     }
 
@@ -72,6 +83,30 @@ public class DeathMountainCrater extends ExitMap {
             intendedAccessibleAsChild(true);
             intendedAccessibleAsAdult(true);
             setOverworld(Overworld.GORON_CITY);
+            setBothSidePositions(new Position(67.0, 56.0));
+            setBothTopPositions(new Position(1.0, 41.5));
+        }
+    }
+
+    @Override
+    public int getPreferredButtonWidth() throws UnknownPerspectiveException {
+        if (Settings.getInstance().getPerspective() == Perspective.SIDE) {
+            return 50;
+        } else if (Settings.getInstance().getPerspective() == Perspective.TOP) {
+            return 45;
+        } else {
+            throw new UnknownPerspectiveException(Settings.getInstance().getPerspective());
+        }
+    }
+
+    @Override
+    public int getPreferredButtonHeight() throws UnknownPerspectiveException {
+        if (Settings.getInstance().getPerspective() == Perspective.SIDE) {
+            return 50;
+        } else if (Settings.getInstance().getPerspective() == Perspective.TOP) {
+            return 45;
+        } else {
+            throw new UnknownPerspectiveException(Settings.getInstance().getPerspective());
         }
     }
 }
