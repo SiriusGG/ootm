3b3418e58dbe8b8de70390ee5d84f73e8a8bf130
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2022-08-16 10:45:47 +0200

    FIX: Made transition between Outside Ganon's Castle and Inside Ganon's Castle unchanging.

--- src/main/java/com/siriusgg/oot/model/places/exits/OutsideGanonsCastleToInsideGanonsCastle.java
+++ src/main/java/com/siriusgg/oot/model/places/exits/OutsideGanonsCastleToInsideGanonsCastle.java
@@ -1,13 +1,15 @@
 package com.siriusgg.oot.model.places.exits;
 
 import com.siriusgg.oot.model.places.*;
+import com.siriusgg.oot.model.places.exitmaps.InsideGanonsCastle;
 
 public class OutsideGanonsCastleToInsideGanonsCastle extends Exit {
     public OutsideGanonsCastleToInsideGanonsCastle(final ExitMap exitMap) {
-        super(exitMap, ExitType.DUNGEON_ENTRANCE);
+        super(exitMap, ExitType.UNCHANGING);
+        setDestinationExitMap(InsideGanonsCastle.class);
         intendedAccessibleAsChild(false);
         intendedAccessibleAsAdult(true);
-        setDungeonEntrance(DungeonEntrance.INSIDE_GANONS_CASTLE);
+        setUnchangingTransition(UnchangingTransition.OUTSIDE_GANONS_CASTLE_TO_INSIDE_GANONS_CASTLE);
         setAdultSidePosition(new Position(53.19, 53.94));
         setAdultTopPosition(new Position(40.05, 33.68));
     }
