76f7ba62a901befafc7523aaa1c7dcc98f692b53
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2022-09-10 19:55:22 +0200

    FIX: Changed entrance position.

--- src/main/java/com/siriusgg/oot/starter/SpecificMapDevStarter.java
+++ src/main/java/com/siriusgg/oot/starter/SpecificMapDevStarter.java
@@ -2,19 +2,20 @@ package com.siriusgg.oot.starter;
 
 import com.siriusgg.oot.controller.CurrentLocationController;
 import com.siriusgg.oot.model.Settings;
-import com.siriusgg.oot.model.places.ExitMap;
-import com.siriusgg.oot.model.places.exitmaps.Graveyard;
-import com.siriusgg.oot.model.util.LAFHandler;
+import com.siriusgg.oot.model.places.*;
+import com.siriusgg.oot.model.places.exitmaps.*;
+import com.siriusgg.oot.model.util.*;
 
 @SuppressWarnings("unused")
 public class SpecificMapDevStarter {
     public static void main(String[] args) {
         LAFHandler.setLAF();
-        // DevTools devTools = DevTools.getInstance();
-        // devTools.addDevMode(DevMode.TRANSITION_BUTTON_DRAGGABLE);
+        DevTools devTools = DevTools.getInstance();
+        devTools.addDevMode(DevMode.TRANSITION_BUTTON_DRAGGABLE);
         String seedName = "devtest";
         Settings s = Settings.getInstance(seedName);
-        ExitMap exitMap = new Graveyard(seedName);
+        s.setPerspective(Perspective.TOP);
+        ExitMap exitMap = new LostWoods(seedName);
         CurrentLocationController clc = new CurrentLocationController(seedName, exitMap);
         clc.init();
         // clc.reposition(2000, 40);
