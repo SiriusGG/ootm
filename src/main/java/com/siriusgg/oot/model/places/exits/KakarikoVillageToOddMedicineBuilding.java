c77fb4a05a5a1a2ddb72561d73951aab4b34f69b
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2023-07-18 20:47:29 +0200

    Set Kakariko Odd Medicine Building (aka Asa's House) to be inaccessible as child.

--- src/main/java/com/siriusgg/oot/model/places/exits/KakarikoVillageToOddMedicineBuilding.java
+++ src/main/java/com/siriusgg/oot/model/places/exits/KakarikoVillageToOddMedicineBuilding.java
@@ -5,7 +5,7 @@ import com.siriusgg.oot.model.places.*;
 public class KakarikoVillageToOddMedicineBuilding extends Exit {
     public KakarikoVillageToOddMedicineBuilding(final ExitMap exitMap) {
         super(exitMap, ExitType.DOOR_ENTRANCE);
-        intendedAccessibleAsChild(true);
+        intendedAccessibleAsChild(false);
         intendedAccessibleAsAdult(true);
         setDoorEntrance(DoorEntrance.KAK_ODD_MEDICINE_BUILDING);
         setBothSidePositions(new Position(56.99, 31.4));
