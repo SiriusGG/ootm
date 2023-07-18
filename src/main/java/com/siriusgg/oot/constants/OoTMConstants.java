768908b64f1e876ecabb099dc9afef7b3e63f62a
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2023-07-18 20:56:23 +0200

    Changed version number from 1.0.0 to 1.1.0

--- src/main/java/com/siriusgg/oot/constants/OoTMConstants.java
+++ src/main/java/com/siriusgg/oot/constants/OoTMConstants.java
@@ -6,7 +6,7 @@ import java.awt.*;
 
 @SuppressWarnings("unused")
 public class OoTMConstants {
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
     public static final String USER_HOME = StringFunctions.toForwardSlashes(System.getProperty("user.home"));
     public static final String ADDITIONAL_CONNECTIONS_DIRECTORY = "graphics/maps/additional_connections";
     public static final String ADULT_ONLY_DIRECTORY = "graphics/maps/adult_only";
